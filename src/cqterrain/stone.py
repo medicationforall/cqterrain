0165c6a4f3ecefdd54d9528f020c092d5ca38e1a
medicationforall <inklink28@gmail.com>
2022-10-30 10:59:01 -0400

    Code comments.

--- src/cqterrain/stone.py
+++ src/cqterrain/stone.py
@@ -3,12 +3,14 @@ import cadquery as cq
 import random
 import math
 
-def make_stones(parts, dim=[5,5,2], rows=2, columns = 5, seed="test4"):
+def make_stones(parts, dim=[5,5,2], rows=2, columns=5, seed="test4"):
     grid = cq.Assembly()
     random.seed(seed)
 
+    # loop the rows
     for row_i in range(rows):
         row_offset = (dim[0] * row_i)
+        # loop the columns per row
         for col_i in range(columns):
             col_offset = (dim[1] * col_i)
 
@@ -20,14 +22,20 @@ def make_stones(parts, dim=[5,5,2], rows=2, columns = 5, seed="test4"):
 
             z_push = random.randrange(-1*math.floor(dim[2]),math.floor(dim[2]))
             z_push=0
+            #move the part in a random direction along the x and y axis.
             x_rand = random.randrange(-1*(math.floor(dim[0]/2)),(math.floor(dim[0]/2)))
             y_rand = random.randrange(-1*(math.floor(dim[1]/2)),(math.floor(dim[1]/2)))
+
+            # choose a random part from the parts list
             part_index = random.randrange(0,len(parts))
+
+            # add the part to the assembly
             grid.add(parts[part_index], loc=cq.Location(cq.Vector(row_offset + col_push_x + x_rand, col_offset + col_push_y + y_rand, z_push)))
 
     length = dim[1] * columns
     width = dim[0] * rows
 
+    # dump the assembly out as a single compound
     comp = grid.toCompound()
     work = cq.Workplane("XZ").center(0, 0).workplane()
     work.add(comp)
