6de8f7116e58bd5b4cbd834e4ef24889bb5363ff
medicationforall <inklink28@gmail.com>
2024-05-10 06:03:58 -0400

    Annotated ladder, fixed one part of a series bug

--- src/cqterrain/Ladder.py
+++ src/cqterrain/Ladder.py
@@ -13,43 +13,61 @@
 # limitations under the License.
 
 import cadquery as cq
-from cadqueryhelper import series
+from cadqueryhelper import series, Base
 import math
+from typing import Callable
 
-def _make_side_rail(width, height, rail_width):
+def _make_side_rail(
+        width:float, 
+        height:float, 
+        rail_width:float
+    ) -> cq.Workplane:
     rail = (cq.Workplane("XY")
-            .box( height, rail_width, width)
+            .box(height, rail_width, width)
             .rotate((0,0,1),(0,0,0),90)
             .rotate((1,0,0),(0,0,0),90)
             )
     return rail
 
-def _make_rung(length, width, height):
+def _make_rung(
+        length:float, 
+        width:float, 
+        height:float
+    ) ->cq.Workplane:
     rung = cq.Workplane("XY").box(length, width ,height)
     return rung
 
-class Ladder:
-    def __init__(self, length=25, width=4, height=50, rail_width=2):
+class Ladder(Base):
+    def __init__(self, 
+            length:float = 25, 
+            width:float = 4, 
+            height:float = 50, 
+            rail_width:float = 2
+        ):
         self.length = length
         self.width = width
         self.height = height
         self.rail_width = rail_width
-        self.rung_height = 2
-        self.rung_width = 2
-        self.rung_padding = 6
+        self.rung_height:float = 2
+        self.rung_width:float = 2
+        self.rung_padding:float = 6
 
         # callbacks
-        self.make_rung = _make_rung
-        self.make_side_rail = _make_side_rail
+        self.make_rung:Callable[[float,float,float],cq.Workplane] = _make_rung
+        self.make_side_rail:Callable[[float,float,float],cq.Workplane] = _make_side_rail
 
         # parts
-        self.outline=None
-        self.side_rails = []
-        self.rung = None
-        self.rungs = None
+        self.outline:cq.Workplane|None=None
+        self.side_rails:list[cq.Workplane] = []
+        self.rung:cq.Workplane|None = None
+        self.rungs:cq.Workplane|None = None
 
     def make(self):
-        self.outline = cq.Workplane("XY").box(self.length, self.width, self.height)
+        self.outline = cq.Workplane("XY").box(
+            self.length, 
+            self.width, 
+            self.height
+        )
         self.__make_rails()
         self.__make_rung()
         self.__make_rungs()
@@ -58,7 +76,10 @@ class Ladder:
         self.rung = self.make_rung(self.length, self.rung_width, self.rung_height)
 
     def __make_rungs(self):
-        rung_count = math.floor(self.height / (self.rung_height + self.rung_padding))
+        rung_count:int = math.floor(self.height / (self.rung_height + self.rung_padding))
+        if not self.rung:
+            raise Exception('self.rung is empty')
+
         self.rungs = series(self.rung, rung_count, height_offset=0 + self.rung_padding)
 
     def __make_rails(self):
